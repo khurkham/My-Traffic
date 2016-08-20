8a7d5d62bfe9a9e93e7de48fe5067a5a76d360b0
masterUNG <phrombutr@gmail.com>
2016-08-20 15:00:13 +0700

    finish 20Aug

--- app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
+++ app/src/main/java/appewtc/masterung/mytraffic/MainActivity.java
@@ -1,13 +1,44 @@
 package appewtc.masterung.mytraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Explicit การประกาศตัวแปร
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); // This's statement
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget คือการผูกความสัมพันธ์ ตัวแปร และ Widget
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+        //Get Event from Click Button
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.effect_btn_long);
+                mediaPlayer.start();
+
+                //Intent to WebView
+
+
+            }   // onClick
+        });
+
+
+    }   // Main Method
+
+}   // Main Class นี่คือ คลาสหลัก เว้ยเห้ย
